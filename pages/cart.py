9659a3e39fa68e33d5840c2b97f3f5f7019b660f
Kody Carling <kodycarling19@gmail.com>
2021-07-07 17:55:12 -0600

    Test is 100% working. Now to refactor..

--- pages/cart.py
+++ pages/cart.py
@@ -6,9 +6,10 @@ class Cart:
     def __init__(self, py: Pylenium):
         self.py = py
 
-        # Locators
-
-        # Actions
+    # Locators
+    CART_REMOVE_ITEM_LINK = "form[method='post'] > div > div:nth-of-type(2)" \
+                                " > .CartItem__Actions.Heading.Text--subdued > .CartItem__Remove.Link"
+    # Actions
 
     def get_list_of_titles_in_suggested_area_of_cart(self):
         text_suggested_products = self.py.get('.Cart__UpsellList a').text()
@@ -16,7 +17,7 @@ class Cart:
         return suggested_products_list
 
     def remove_all_items_from_cart(self):
-        self.py.get("form[method='post'] > div > div:nth-of-type(2) > .CartItem__Actions.Heading.Text--subdued > .CartItem__Remove.Link").click(force=True)
-        self.py.wait(use_py=True).sleep(2)
-        self.py.get("form[method='post'] > div > div:nth-of-type(2) > .CartItem__Actions.Heading.Text--subdued > .CartItem__Remove.Link").click(
+        self.py.get(self.CART_REMOVE_ITEM_LINK).click(force=True)
+        self.py.wait(use_py=True).sleep(1)
+        self.py.get(self.CART_REMOVE_ITEM_LINK).click(
             force=True)
--- tests/ui_tests/test_pura_demo.py
+++ tests/ui_tests/test_pura_demo.py
@@ -17,28 +17,34 @@ def test_show_pura_my_coding_skills(pura):
     # Add any available fragrance as one - time purchase
     pura.py.get(".Cart__UpsellList a[href*='maui']").click(force=True)
     pura.py.get("#one_time").click()
+    one_time_price = int(pura.py.get("#one_time_price_container").text()[1:])
     pura.py.get("button[data-action='add-to-cart']").click()
     # Add the same fragrance as subscription
     pura.py.get("button[aria-label='Close cart']").click(force=True)
     pura.py.get("#recurring").click()
+    recurring_price = float(pura.py.get("#rc_price_autodeliver").text()[1:])
     pura.py.get("button[data-action='add-to-cart']").click()
+    sub_total_control = one_time_price + recurring_price
     # Verify the price of fragrance is different in comparison with one - time purchase and subscription
-    '''Some Kind of Assert'''
+    assert recurring_price < one_time_price
     # Go to Checkout Check the sum is correct
     pura.py.get("#cart-drawer-checkout-button").click(force=True)
-    '''Some Kind of Assert'''
+    sub_total_actual = float(pura.py.get("#payment_due_amount").text()[1:])
+    assert sub_total_control == sub_total_actual
     # Provide email address
     pura.py.get("#checkout_email").type("kodycarling19@testgmail.com")
     # Apply discount code VIP3Z09T7GQ9 - 20 % discount
     pura.py.get("#show-discount-form-link").click()
     pura.py.get("#checkout_discount_code").type("PURASPRING")
     pura.py.get("#apply_discount").click()
+    discount_amount = float(format(sub_total_actual*.15, '.2f'))
+    pura.py.wait(use_py=True).sleep(2)
+    sub_total_control_after_discount_applied = float(pura.py.get("#payment_due_amount").text()[1:])
     # Verify the sum is recalculated correctly
-    '''Some kind of Assert'''
+    assert sub_total_control_after_discount_applied == round(sub_total_actual-discount_amount, 2)
     # Do not continue to Checkout, Go back to Cart
     pura.py.get("#continue-button").scroll_into_view()
     pura.py.webdriver.find_element_by_link_text("< Return to cart").click()
     # remove all items from the cart Verify the Cart is empty
     pura.cart.remove_all_items_from_cart()
     assert "Your cart is empty" in pura.py.get(".EmptyState").text()
-    pura.py.wait(use_py=True).sleep(5)
